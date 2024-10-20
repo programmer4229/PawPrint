4d6109f70f743be1b75eb392e21befaaef594537
programmer4229 <ggonzalez77@gatech.edu>
2024-10-20 15:39:20 -0400

    routing for pages, using react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/SignIn';
 import PetProfile from './components/PetProfile';
+import PetSelectionPage from './components/ProfileSelection';
+
 
 function App() {
   return <SignIn />;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './components/SignIn';
+import PetSelectionPage from './components/ProfileSelection';
+import PetProfile from './components/PetProfile';
+import { RouterProvider } from 'react-router-dom';
+
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {path: '/' , element: <App />},
+    {path: '/signin', element: <SignIn />},
+    {path: '/petselection', element: <PetSelectionPage />},
+    {path: '/petprofile', element: <PetProfile />},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
