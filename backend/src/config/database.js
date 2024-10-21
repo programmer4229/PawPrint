a868292fa90e7b78ab7099dc485f1bb59fc08ae3
aassa7 <aassa7@gatech.edu>
2024-10-21 12:56:26 -0400

    Updated database configuration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: ':memory:',
+    dialect: 'postgres',
+    host: 'database-1.cd6ky6cymg3c.us-east-2.rds.amazonaws.com',
+    username: 'admin_test',
+    password: 'AlmostFullAdmin07?',
+    database: 'testdb',
+    'port': 54327,
     logging: false,
   });
   
